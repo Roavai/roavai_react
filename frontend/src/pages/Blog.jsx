f14932bcb6a3c5463c8d1511f0b8b475725c1c1d
VenkateshBhatOP <bhatvenkatesh6@gmail.com>
2026-01-02 20:00:33 +0530

    Merge main into venki_main and fix lint errors/warnings

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,7 +1,8 @@
 // src/pages/Blog.jsx
 import { useMemo } from 'react'
 import { Link } from 'react-router-dom'
-import { motion } from 'framer-motion'
+import { useMemo } from 'react'
+import { Link } from 'react-router-dom'
 import { posts } from '../utils/blogData'
 import BlogPostCard from '../components/BlogPostCard'
 
